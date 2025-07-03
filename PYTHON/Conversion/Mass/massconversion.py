8d5ea868a945ecddbd35486c7c07fb0d3569ca10
divyanshg221220 <divyanshg221220@gmail.com>
2025-07-03 22:52:38 +0530

    .

---
+++ PYTHON/Conversion/Mass/massconversion.py
@@ -0,0 +1,11 @@
+#massconversion
+def kgtotonne(n):
+    return n*0.001
+def tonnetokg(n):
+    return n/0.001
+def kgtopound(n):
+    return n*2.20462
+def poundtokg(n):
+    return n/2.20462
+def help():
+    return "1 kg = 0.001 tonne, 1 kg = 2.20462 pound"
---
+++ PYTHON/Conversion/lengthconversion.py
@@ -0,0 +1,11 @@
+#lengthconversion
+def miletokm(n):
+    return n*1.609344
+def kmtomile(n):
+    return n/1.609344
+def feettoinches(n):
+    return n*12
+def inchestofeet(n):
+    return n/12
+def help():
+    return "1 mile = 1.609344 kilometer ; 1 feet = 12 inches"
